544adf49cddade02bf77c215cde3c096663b96c3
alanpgs <alan@greensmedia.com>
2022-09-20 17:18:49 +0400

    firt commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { resolve, basename } from 'path';
+
+export default defineConfig({
+    server: {
+        port: 9090
+    },
+    build: {
+        outDir: '../dist'
+    },  
+    publicDir: 'src',  
+})
